b841e6b6042b5d7b6e98b8f939cb3730122a68ed
Adam Ma <adam.zym@gmail.com>
2024-12-03 12:43:55 -0500

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages';
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
