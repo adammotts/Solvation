bdf18cd51515c5390acd7ae6db809ca93a72153b
Adam Ma <adam.zym@gmail.com>
2024-12-23 19:49:39 -0500

    Verify Move To Dev

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,5 @@
 using Solvation.Algorithms;
 
-Solver.VerifyInteractions();
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
@@ -21,6 +19,7 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    Solver.VerifyInteractions();
     app.MapOpenApi();
 }
 
